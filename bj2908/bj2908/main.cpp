a796bf7c8b93ddbb58814d30e73139e4eeddcfa1
LimSH <lsh00731@gmail.com>
2021-10-14 16:28:04 +0900

    bj2908

---
+++ bj2908/bj2908/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+
+	int a = 0, b = 0;
+
+	string n1, n2, n3;
+
+	getline(cin, n1);
+
+	reverse(n1.begin(), n1.end());
+
+	int num = n1.find(' ');
+
+	n2 = n1.substr(0, num);
+	n3 = n1.substr(num + 1);
+
+	a = stoi(n2);
+	b = stoi(n3);
+
+	cout << (a > b ? a : b);
+
+
+
+	return 0;
+}
